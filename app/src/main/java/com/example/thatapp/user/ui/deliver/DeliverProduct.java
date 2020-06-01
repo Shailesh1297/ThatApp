0f3b0e02ba7f784a82ac62a1f1ab3a18266d3d04
shailesh <uic.17bca1297@gmail.com>
2020-06-01 13:24:13 +0530

    basic elements

---
+++ app/src/main/java/com/example/thatapp/user/ui/deliver/DeliverProduct.java
@@ -0,0 +1,29 @@
+package com.example.thatapp.user.ui.deliver;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.thatapp.R;
+
+
+public class DeliverProduct extends Fragment {
+
+
+
+    public DeliverProduct() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_deliver_product, container, false);
+    }
+}
